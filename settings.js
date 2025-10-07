23ca77af56d6cf331b0aaed0201f6ac8b624f43b
Rahim22 <rahimbjunior@gmail.com>
2025-10-07 15:39:38 +0300

    Update settings.js for Rahimbjunior

--- settings.js
+++ settings.js
@@ -1,8 +1,6 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
-  CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
+  SESSION_ID: "SUBZERO-MD;;;XQ82xYpS#vs8v8Zm7ML4QANSxUiP6ZWECJgtKN62mhD6rgZDSAqA",
+  OWNER_NUMBER: "+256787242368", 
+  PREFIX: "/",
+  CDN: "https://mrfrankk-cdn.hf.space"
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
